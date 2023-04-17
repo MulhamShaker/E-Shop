cca91a8b93b46daff799f5964fa17f354a12c810
molham-shaker <molham.sh95@gmail.com>
2023-04-17 06:02:52 +0200

    update

--- shopifyshop/pages/models.py
+++ shopifyshop/pages/models.py
@@ -3,3 +3,10 @@ from django.db import models
 class Login(models.Model):
     username = models.CharField(max_length=15)
     password = models.CharField(max_length=15)
+    
+class Signup(models.Model):
+    firstname = models.CharField(max_length=15)
+    lastname = models.CharField(max_length=15)
+    password = models.CharField(max_length=15)
+    email = models.EmailField()
+    
--- shopifyshop/pages/views.py
+++ shopifyshop/pages/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from .models import Login
+from .models import Signup
 
 
 def home(request):
     return render (request, "pages/home.html")
     
-
 
 
 def login(request):
@@ -18,3 +18,18 @@ def login(request):
     print(username , password)
     data = Login(username = username , password = password)
     return render(request, "pages/login.html")
+
+
+
+def signup(request):
+    if request.method == "POST":
+        data = Signup(request.POST)
+        if data == True:
+            data.save()
+    firstname = request.POST.get("firtsname")
+    lastname = request.POST.get("lastname")
+    password = request.POST.get("password")
+    email = request.POST.get("email")
+    print(firstname,lastname, password, email)
+    data = Signup(firstname = firstname, lastname = lastname, password = password, email = email)
+    return render(request, "pages/signup.html")
